6f254cd88c5c4000db89c17842fcc68e8448c98f
rolandqv <rolando.quiza@gmail.com>
2024-09-05 12:01:28 -0500

    Ejemplo de variables

---
+++ Sesión 02/test_01.py
@@ -0,0 +1,13 @@
+"""Creando variables"""
+"""Enteros: int"""
+"""Flotantes: float"""
+
+var1 = 40
+var2 = 105
+var3 = 60.3
+var4 = -10.56
+
+print("El valor de var1 es: ",var1)
+print("El valor de var2 es: ",var2)
+print("El valor de var3 es: {}".format(var3))
+print("El valor de var4 es: {}".format(var4))
---
+++ Sesión 02/test_02.py
@@ -0,0 +1,16 @@
+"""Datos Boolean"""
+"""Booleanos: bool"""
+
+var1 = True
+var2 = False
+
+var3 = []
+var4 = 0
+
+print("El valor de var3 es: {}".format(var1))
+print("El valor de var3 es: {}".format(var2))
+
+if var1:
+    print("El valor de var3 es: {}".format(var1))
+if not var2:
+    print("El valor de var3 es: {}".format(var2))
